b313d70aea74f125419d9420f247cd3551deb853
Default user <vojtech.kase@gmail.com>
2020-09-08 10:18:24 +0000

    aoristic analysis

--- scripts/modelling_distributions.py
+++ scripts/modelling_distributions.py
@@ -1,89 +1,82 @@
 from scipy.stats import trapz
 from scipy.stats import halfnorm
 
-def dist_range(start, stop, size=1, b=0.1): # this function has been implemented into already modelling_distributions.py
-  """
-  get random numbers of size size ib on the basis of start date and end date and trapezoid distribution defined by first turn point (lower bound)
-  """
-  duration = abs(start-stop)
-  if duration == 0:
-    random_values = [start] * size
-    return random_values
-  else:
+def dist_range(start, stop, size=1, b=0): 
+    """
+    get random numbers of size = size on the basis of a start date and an end date
+    by default: uniform distribution
+    by specifying b parameter you get a trapezoidal distribution defined by the first turning point (lower bound)
+    """
     r = trapz.rvs(b, 1-b, size=size)
-    if 0 in range(start, stop):
-      random_values_with_0 = list(np.round((r * (duration - 1)) + start).astype(int))
-      random_values = []
-      for value in random_values_with_0:
-        if value < 0:
-          random_values.append(value)
-        else:
-          random_values.append(value + 1)
+    duration = abs((start)-stop)
+    if duration == 0:
+        random_values = [start] * size
+        return random_values
     else:
-      random_values = list(np.round((r * duration) + start).astype(int))
+        random_values = list(((r * duration) + start).round().astype(int))
     if size == 1: # if only one number, return it as a number
-      return random_values[0]
+        return random_values[0]
     else: # otherwise return a list of values
-      return random_values
+        return random_values
 
-def dist_ante_post(date, date_type, size=1, scale=25): # this function has been implemented into already modelling_distributions.py
-  """
-  get random numbers of size size ib on the basis of start date and end date and trapezoid distribution defined by first turn point (lower bound)
-  """
-  if "post" in date_type:
-    r = halfnorm.rvs(date, scale, size)
-    random_values = list(r.astype(int))
-  if "ante" in date_type:
-    r = halfnorm.rvs(scale=scale, size=size)
-    random_values =  list((date - r).astype(int))
-  if 0 in random_values:
-     random_values_without_0 = []
-     for value in random_values:
-        if value < 0:
-          random_values_without_0.append(value)
-        else:
-          random_values_without_0.append(value + 1)
-     random_values = random_values_without_0
-  if size == 1:
-    return random_values[0]
-  else:
-    return random_values
+def dist_ante_post(date, date_type, size=1, scale=50): # this function has been already implemented into modelling_distributions.py
+    """
+    get random numbers of size size ib on the basis of start date and end date and trapezoid distribution defined by first turn point (lower bound)
+    """
+    if "post" in date_type:
+        r = halfnorm.rvs(date, scale, size)
+        random_values = list(r.astype(int))
+    if "ante" in date_type:
+        r = halfnorm.rvs(scale=scale, size=size)
+        random_values =  list((date - r).astype(int))
+    if 0 in random_values:
+        random_values_without_0 = []
+        for value in random_values:
+            if value < 0:
+                random_values_without_0.append(value)
+            else:
+                random_values_without_0.append(value + 1)
+        random_values = random_values_without_0
+    if size == 1:
+        return random_values[0]
+    else:
+        return random_values
 
 
-def model_date(start, stop, size=1, scale=25, b=0.1):
+def model_date(start, stop, size=1, scale=25, b=0):
     """
     combine dist_range() and dist_ante_post()
     """
     try:
-      randoms = dist_range(int(start), int(stop), size=size, b=b)
+        randoms = dist_range(int(start), int(stop), size=size, b=b)
     except:
-      try:
-        randoms =  dist_ante_post(int(start), "post", size=size, scale=scale)
-      except:
         try:
-          randoms =  dist_ante_post(int(stop), "ante", size=size, scale=scale)
+            randoms =  dist_ante_post(int(start), "post", size=size, scale=scale)
         except:
-          randoms = None
+            try:
+                randoms =  dist_ante_post(int(stop), "ante", size=size, scale=scale)
+            except:
+                randoms = None
     return randoms
 
 
 
 def get_simulation_variants(dataframe, column):
-  """
-  combine random dates associated with individual observations
-  into a list of simulations
-  each simulation consists of a list containing one version of dates
-  """
-  random_dates_list = dataframe[column].tolist()
-  simulations_list = []
-  if isinstance(random_dates_list[0], list):
-    random_size = len(random_dates_list[0])
-  else:
-    random_size = len(random_dates_list[1])
-  for n in range(random_size):
-    simulation = [random_dates[n] for random_dates in random_dates_list if isinstance(random_dates, list)]
-    simulations_list.append(simulation)
-  return simulations_list
+    """
+    combine random dates associated with individual observations
+    into a list of simulations
+    each simulation consists of a list containing one version of dates
+    """
+    random_dates_list = dataframe[column].tolist()
+    simulations_list = []
+    if isinstance(random_dates_list[0], list):
+        random_size = len(random_dates_list[0])
+    else:
+        random_size = len(random_dates_list[1])
+    for n in range(random_size):
+        simulation = [random_dates[n] for random_dates in random_dates_list if isinstance(random_dates, list)]
+        simulations_list.append(simulation)
+    return simulations_list
 
 
 def dates_per_block(list_of_dates, time_blocks):
@@ -126,3 +119,24 @@ def plot_timeblocks_data(list_of_timeblocks_data, ax=None):
       ax.plot(x, y)
     else:
       plt.plot(x, y)
+
+def get_aoristic(startdate, enddate, timeblocks_tuples):
+    aoristic_probs = {}
+    try:
+        startdate, enddate = int(startdate), int(enddate)
+        ind_year_prob = np.round(1 / len([n for n in range(startdate, enddate)]), 5)
+        for timeblock in timeblocks_tuples:
+            possibledates = [n for n in range(startdate, enddate)]
+            timeblock_range = [n for n in range(timeblock[0], timeblock[1])]
+            aoristic_probs[timeblock] = len(set(possibledates) & set(timeblock_range)) * ind_year_prob
+    except:
+        for timeblock in timeblocks_tuples:
+            aoristic_probs[timeblock] = 0
+    return aoristic_probs    
+
+
+def get_aoristic_sum(prob_dicts_list, timeblocks_tuples):
+    aoristic_sum = {}
+    for timeblock in timeblocks_tuples:
+        aoristic_sum[timeblock] =  sum([probs[timeblock] for probs in prob_dicts_list])
+    return aoristic_sum
